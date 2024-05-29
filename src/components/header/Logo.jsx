7689c6fb6e5f1e07efe013b59112bc0a34233f31
hyunminkim93 <rlagusals234@gmail.com>
2024-05-29 18:46:53 +0900

    chicken

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { GiChicken } from 'react-icons/gi'
-import { Link } from 'react-router-dom'
 
 const Logo = () => {
     return (
         <h1 className='header__logo'>
-            {/* Link 컴포넌트 사용 */}
-            <Link to="/">
+            <a href="/">
                 <em><GiChicken /></em>
                 <span>치킨 브랜드 모음집</span>
-            </Link>
+            </a>
         </h1>
     )
 }
